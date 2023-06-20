94609d919ccb36740e1c212d9a19ee6aeffad015
Gohcar <glauberrrocha@gmail.com>
2023-06-20 16:53:26 -0300

    Sintaxe Java Basico

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor digite o numero da Conta");
+        numero = sc.nextInt();
+        System.out.println("Por favor digite o numero da Agencia");
+        agencia = sc.next();
+        System.out.println("Por favor digite Nome e Sobrenome");
+        nomeCliente = sc.next();
+        System.out.println("Por favor digite o seu Saldo");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero +" e seu saldo " + saldo + " já está disponível para saque.");
+    }
+   
+}
